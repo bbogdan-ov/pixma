9f686791b31e36c498db80a713680a3052e3379e
bogdanov <bbogdan_ov@mail.ru>
2024-02-02 21:46:41 +0300

    a kind of rewrite of the `Color` class for easier use

--- src/base/common/misc/Color.ts
+++ src/base/common/misc/Color.ts
@@ -1,23 +1,22 @@
-import { Random, ColorUtils } from "@base/utils";
+import { Random, ColorUtils, Utils } from "@base/utils";
 import { HexColor, HslColor, HslStringColor, HsvColor, RgbColor, RgbStringColor } from "@base/types/types";
 
-/** @deprecated */
 export default class Color {
-    protected _rgb: RgbColor;
-    protected _hex: HexColor;
-    protected _hsl: HslColor;
-    protected _hsv: HsvColor;
+    protected _red!: number;
+    protected _green!: number;
+    protected _blue!: number;
+    protected _hue!: number;
+    protected _saturation!: number;
+    protected _value!: number;
+    protected _hex!: HexColor;
     protected _alpha: number = 1;
     
-    constructor(rgb: RgbColor) {
-        this._rgb = rgb;
-        this._hex = this.getHex();
-        this._hsl = this.getHsl();
-        this._hsv = this.getHsv();
+    constructor(...rgb: RgbColor) {
+        this.setRgb(...rgb);
     }
 
     copy(color: Color): this {
-        this.setRgb([...color.rgb]);
+        this.setRgb(...color.rgb);
         return this;
     }
     clone(): Color {
@@ -25,67 +24,87 @@ export default class Color {
     }
 
     // Set
-    setRgb(rgb: RgbColor): this {
-        this._rgb = rgb;
+    setRgb(...rgb: RgbColor): this {
+        // TODO: clamp RGB
+
+        this._red = rgb[0];
+        this._green = rgb[1];
+        this._blue = rgb[2];
+
+        const hsv = this.getHsv();
+        this._hue = hsv[0];
+        this._saturation = hsv[1];
+        this._value = hsv[2];
+        
         this._hex = this.getHex();
-        this._hsl = this.getHsl();
-        this._hsv = this.getHsv();
+
+        return this;
+    }
+    setHsv(...hsv: HsvColor): this {
+        // TODO: clamp HSV
+
+        const rgb = ColorUtils.hsvToRgb(hsv);
+        this._red = rgb[0];
+        this._green = rgb[1];
+        this._blue = rgb[2];
+
+        this._hue = hsv[0];
+        this._saturation = hsv[1];
+        this._value = hsv[2];
+
+        this._hex = this.getHex();
+
         return this;
     }
     setHex(hex: HexColor): this {
-        this._rgb = ColorUtils.hexToRgb(hex);
-        // TODO: format hex
+        // TODO: format HEX
+
+        const rgb = ColorUtils.hexToRgb(hex);
+        this._red = rgb[0];
+        this._green = rgb[1];
+        this._blue = rgb[2];
+
+        const hsv = this.getHsv();
+        this._hue = hsv[0];
+        this._saturation = hsv[1];
+        this._value = hsv[2];
+
         this._hex = hex;
-        this._hsl = this.getHsl();
-        this._hsv = this.getHsv();
-        return this;
-    }
-    setHsl(hsl: HslColor): this {
-        this._rgb = ColorUtils.hslToRgb(hsl);
-        this._hex = this.getHex();
-        this._hsl = hsl;
-        this._hsv = this.getHsv();
-        return this;
-    }
-    setHsv(hsv: HsvColor): this {
-        this._rgb = ColorUtils.hsvToRgb(hsv);
-        this._hex = this.getHex();
-        this._hsl = this.getHsl();
-        this._hsv = hsv;
+
         return this;
     }
     setAlpha(value: number): this {
-        this._alpha = value;
+        this._alpha = Utils.clamp(value, 0, 1);
         return this;
     }
     setRed(value: number): this {
-        return this.setRgb([value, this.green, this.blue]);
+        return this.setRgb(value, this.green, this.blue);
     }
     setGreen(value: number): this {
-        return this.setRgb([this.red, value, this.blue]);
+        return this.setRgb(this.red, value, this.blue);
     }
     setBlue(value: number): this {
-        return this.setRgb([this.red, this.blue, value]);
+        return this.setRgb(this.red, this.blue, value);
     }
     setHue(value: number): this {
-        return this.setHsv([value, this.saturation, this.value]);
+        return this.setHsv(value, this.saturation, this.value);
     }
     setSaturation(value: number): this {
-        return this.setHsv([this.hue, value, this.value]);
+        return this.setHsv(this.hue, value, this.value);
     }
     setValue(value: number): this {
-        return this.setHsv([this.hue, this.saturation, value]);
+        return this.setHsv(this.hue, this.saturation, value);
     }
 
     // Get
     getHex(): HexColor {
-        return ColorUtils.rgbToHex(this._rgb);
+        return ColorUtils.rgbToHex(this.rgb);
     }
     getHsl(): HslColor {
-        return ColorUtils.rgbToHsl(this._rgb);
+        return ColorUtils.rgbToHsl(this.rgb);
     }
     getHsv(): HsvColor {
-        return ColorUtils.rgbToHsv(this._rgb);
+        return ColorUtils.rgbToHsv(this.rgb);
     }
 
     /** @alias color.getRgbString() */
@@ -93,41 +112,38 @@ export default class Color {
         return this.getRgbString();
     }
     getRgbString(): RgbStringColor {
-        return ColorUtils.rgbToString(this._rgb) ;
+        return ColorUtils.rgbToString(this.rgb);
     }
     getHslString(): HslStringColor {
         return ColorUtils.hslToString(this.getHsl());
     }
 
     get rgb(): RgbColor {
-        return this._rgb.slice() as RgbColor;
+        return [this.red, this.green, this.blue];
     }
     get hex(): HexColor {
         return this._hex;
     }
-    get hsl(): HslColor {
-        return this._hsl.slice() as HslColor;
-    }
     get hsv(): HsvColor {
-        return this._hsv.slice() as HsvColor;
+        return [this.hue, this.saturation, this.value];
     }
     get red(): number {
-        return this._rgb[0];
+        return this._red;
     }
     get green(): number {
-        return this._rgb[1];
+        return this._green;
     }
     get blue(): number {
-        return this._rgb[2];
+        return this._blue;
     }
     get hue(): number {
-        return this.hsv[0];
+        return this._hue;
     }
     get saturation(): number {
-        return this.hsv[1];
+        return this._saturation;
     }
     get value(): number {
-        return this.hsv[2];
+        return this._value;
     }
     get alpha(): number {
         return this._alpha;
@@ -135,33 +151,33 @@ export default class Color {
 
     // Static
     static from(color: Color): Color {
-        return new Color(color.rgb);
+        return new Color(...color.rgb);
     }
     static random(): Color {
-        return new Color([Random.int(0, 255), Random.int(0, 255), Random.int(0, 255)]);
+        return new Color(Random.int(0, 255), Random.int(0, 255), Random.int(0, 255));
     }
     static fromHex(hex: HexColor): Color {
-        return new Color(ColorUtils.hexToRgb(hex));
+        return new Color(...ColorUtils.hexToRgb(hex));
     }
     static fromHsl(hsl: HslColor): Color {
-        return new Color(ColorUtils.hslToRgb(hsl));
+        return new Color(...ColorUtils.hslToRgb(hsl));
     }
     static fromHsv(hsv: HsvColor): Color {
-        return new Color(ColorUtils.hsvToRgb(hsv));
+        return new Color(...ColorUtils.hsvToRgb(hsv));
     }
     static get BLACK(): Color {
-        return new Color([0, 0, 0]);
+        return new Color(0, 0, 0);
     }
     static get WHITE(): Color {
-        return new Color([255, 255, 255]);
+        return new Color(255, 255, 255);
     }
     static get RED(): Color {
-        return new Color([255, 0, 0]);
+        return new Color(255, 0, 0);
     }
     static get GREEN(): Color {
-        return new Color([0, 255, 0]);
+        return new Color(0, 255, 0);
     }
     static get BLUE(): Color {
-        return new Color([0, 0, 255]);
+        return new Color(0, 0, 255);
     }
 }
