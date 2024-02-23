bd6c9a940455498ef449acc9f56c02e3e2d54dd2
bogdanov <bbogdan_ov@mail.ru>
2024-02-24 00:20:48 +0300

    listen palette changes

--- src/elements/panels/PalettePanel.ts
+++ src/elements/panels/PalettePanel.ts
@@ -1,16 +1,37 @@
-import { Panel } from "@base/elements/panels";
+import { Panel, PanelContent } from "@base/elements/panels";
 import { Orientation } from "@base/types/enums";
+import { DOM } from "@base/utils";
+import type { PaletteColor } from "@source/common/colors";
 import type { Project } from "@source/common/project";
 
 @Panel.define("palette-panel")
 export default class PalettePanel extends Panel {
     readonly project: Project;
+
+    readonly colorsList = DOM.div("colors-list");
     
     constructor(project: Project) {
         super(Orientation.VERTICAL);
 
         this.project = project;
         
-        this.classList.add("palette-panel")
+        this.classList.add("palette-panel");
+
+        this.append(new PanelContent(this.colorsList));
+
+        for (const color of project.palette.list) {
+            this.colorsList.append(color.createElement());
+        }
+    }
+
+    // On
+    onMount(): void {
+        super.onMount();
+
+        this.listen(this.project.palette.onDidAdded, this._onPaletteColorAdd.bind(this));
+    }
+    protected _onPaletteColorAdd(color: PaletteColor) {
+        // TODO: sorting colors in right order (like in the palette list itself)
+        this.colorsList.append(color.createElement());
     }
 }
