3e032b84f393bc8bf7190fee1004cb2be253c1b6
bogdanov <bbogdan_ov@mail.ru>
2024-02-24 00:22:19 +0300

    changing color

--- src/elements/colors/PaletteColorElement.ts
+++ src/elements/colors/PaletteColorElement.ts
@@ -1,10 +1,27 @@
 import { BaseElement } from "@base/elements";
+import { EventName } from "@base/types/enums";
+import { PaletteColor } from "@source/common/colors";
 
 @BaseElement.define("palette-color")
 export default class PaletteColorElement extends BaseElement {
-    constructor() {
+    readonly paletteColor: PaletteColor;
+    
+    constructor(paletteColor: PaletteColor) {
         super();
 
+        this.paletteColor = paletteColor;
+        this.setStyle("background", paletteColor.getRgbString());
+
         this.classList.add("palette-color");
+    }
+
+    // On
+    onMount(): void {
+        super.onMount();
+
+        this.listen(this, EventName.DOWN, this._onDown.bind(this));
+    }
+    protected _onDown() {
+        this.paletteColor.pick();
     }
 }
