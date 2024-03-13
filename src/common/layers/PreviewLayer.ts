2f092866cba5d2954501e51c15ac786b31b4f8db
bogdanov <bbogdan_ov@mail.ru>
2024-03-13 22:45:39 +0300

    better way to draw preview

--- src/common/layers/PreviewLayer.ts
+++ src/common/layers/PreviewLayer.ts
@@ -8,16 +8,59 @@ export default class PreviewLayer extends Layer {
     
     constructor(manager: LayersManager) {
         super(PreviewLayer.NAME, manager);
+
+        this.listen(manager.onDidChosen, ()=> {
+            this.drawCurrentLayer();
+        })
+    }
+
+    draw(tool: Tool | null, mouse: IMouseData | null) {
+        this.clear();
+        this.drawCurrentLayer();
+
+        if (tool && mouse && this.getIsDrawPreview()) {
+            tool.drawPreview(this, mouse);
+        }
+    }
+    
+    drawCurrentLayer() {
+        const layer = this.manager.current;
+        if (!layer) return;
+
+        // Don't draw and show current layer's canvas
+        if (this.isToolDown) {
+            layer.setCanvasElementVisibility(layer.isVisible);
+            return;
+        }
+
+        // Hide current layer's canvas
+        layer.setCanvasElementVisibility(false);
+
+        // Draw current layer
+        this.clear();
+        this.context.drawImage(layer.canvas.element, 0, 0);
     }
 
     // On
+    onToolDown(tool: Tool, mouse: IMouseData): void {
+        super.onToolDown(tool, mouse);
+
+        this.draw(tool, mouse);
+    }
     onToolMove(tool: Tool, mouse: IMouseData): void {
         super.onToolMove(tool, mouse);
-        const canvasZoomable = this.manager.project.canvasZoomable;
 
-        this.clear();
+        this.draw(tool, mouse);
+    }
+    onToolUp(tool: Tool, mouse: IMouseData): void {
+        super.onToolUp(tool, mouse);
 
-        if (canvasZoomable?.isMouseDown ? true : canvasZoomable?.isMouseOver)
-            tool.drawPreview(this, mouse);
+        this.draw(tool, mouse);
+    }
+
+    // Get
+    getIsDrawPreview(): boolean {
+        const zoomable = this.manager.project.canvasZoomable;
+        return zoomable?.isMouseDown || zoomable?.isMouseOver || false;
     }
 }
