0593349adb6c6b495221e7ca647f786c4128d751
bogdanov <bbogdan_ov@mail.ru>
2024-03-27 22:10:59 +0300

    add pixel perfect toggle

--- src/elements/tools-params/DrawingToolParams.ts
+++ src/elements/tools-params/DrawingToolParams.ts
@@ -1,9 +1,17 @@
+import { CheckboxToggle } from "@base/elements/toggles";
 import { ToolParams } from ".";
-import type { Tool } from "@source/common/tools";
+import type { DrawingTool } from "@source/common/tools";
 
 @ToolParams.define("drawing-tool-params")
-export default class DrawingToolParams<T extends Tool=Tool> extends ToolParams<T> {
+export default class DrawingToolParams<T extends DrawingTool=DrawingTool> extends ToolParams<T> {
+	readonly pixelPerfectToggle: CheckboxToggle; 
+
     constructor(tool: T) {
         super(tool);
+
+		this.pixelPerfectToggle = new CheckboxToggle(tool.isPixelPerfectState);
+
+		this.classList.add("drawing-tool-params");
+		this.append(this.pixelPerfectToggle);
     }
 }
