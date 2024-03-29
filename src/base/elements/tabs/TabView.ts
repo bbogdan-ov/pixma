dfd29bdaf6dea7df12e64620928c6818aa71c104
bogdanov <bbogdan_ov@mail.ru>
2024-03-29 22:25:41 +0300

    add tab listeners for `TabView`

--- src/base/elements/tabs/TabView.ts
+++ src/base/elements/tabs/TabView.ts
@@ -16,8 +16,13 @@ export default class TabView extends BaseElement {
     onMount(): void {
         super.onMount();
 
+		// Just call it here cos when the tab is entered, the view is mounted
+		this._onTabEnter();
         this.listen(this.tab.onDidLeaved, this._onTabLeave.bind(this));
+        this.listen(this.tab.onDidClosed, this._onTabClose.bind(this));
     }
+	protected _onTabEnter() {}
+	protected _onTabClose() {}
     protected _onTabLeave() {
         this.remove();
     }
