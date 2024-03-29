41c3bb5740dd0b81d3489ce38e72e492944dff74
bogdanov <bbogdan_ov@mail.ru>
2024-03-29 18:20:27 +0300

    create `HexInput`

---
+++ src/base/elements/inputs/HexInput.ts
@@ -0,0 +1,31 @@
+import { State } from "@base/common/listenable";
+import { BaseInput } from ".";
+import { Color } from "@base/common/misc";
+
+@BaseInput.define("hex-input")
+export default class HexInput extends BaseInput<string> {
+	protected _showHashChar = true;
+
+	constructor(state: State<string>) {
+		super("#000000", state);
+
+		this.classList.add("hex-input");
+	}
+
+    formatInputValue(value: string | number): string {
+        return value.toString();
+    }
+    formatDisplayValue(value: string): string {
+		const v = Color.formatHex(value);
+
+		if (!this._showHashChar)
+			return v.slice(1);
+        return v
+    }
+	
+	// Set
+	hideHashChar(hidden=true): this {
+		this._showHashChar = !hidden;
+		return this;
+	}
+}
--- src/base/elements/inputs/index.ts
+++ src/base/elements/inputs/index.ts
@@ -1,9 +1,11 @@
 import BaseInput from "./BaseInput"
 import TextInput from "./TextInput"
 import NumberInput from "./NumberInput"
+import HexInput from "./HexInput"
 
 export {
     BaseInput,
     TextInput,
-    NumberInput
+    NumberInput,
+	HexInput
 }
