aa39997161fe7c8124037ec82518b664c8a8c7ad
bogdanov <bbogdan_ov@mail.ru>
2024-04-23 23:08:02 +0300

    prepare for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
 
 export default defineConfig({
+	base: "/pixma/",
     plugins: [
         tsconfigPaths(),
         checker({ typescript: true })
